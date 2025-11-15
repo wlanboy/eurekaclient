8780b8c1910f487b8bb4daaad3ae2378411178ed
wlanboy <wlanboy@github.com>
2025-11-15 14:38:15 +0100

    added actuator info generator

---
+++ eurekaclientjava/src/main/java/com/example/eurekaclient/actuator/EurekaClientsInfoContributor.java
@@ -0,0 +1,24 @@
+package com.example.eurekaclient.actuator;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import com.example.eurekaclient.services.LifecycleManager;
+
+@Component
+public class EurekaClientsInfoContributor implements InfoContributor {
+
+    private final LifecycleManager lifecycleManager;
+
+    public EurekaClientsInfoContributor(LifecycleManager lifecycleManager) {
+        this.lifecycleManager = lifecycleManager;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        int running = lifecycleManager.getRunningInstances().size();
+        builder.withDetail("eurekaClients", running + " Instanzen laufen");
+    }
+}
+
