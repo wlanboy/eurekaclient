f5c3cb71cd9cd784756316d19a402038444efb3f
wlanboy <wlanboy@github.com>
2025-11-15 09:20:19 +0100

    added list of active clients vs configured clients

--- eurekaclientjava/src/main/java/com/example/eurekaclient/web/ClientViewController.java
+++ eurekaclientjava/src/main/java/com/example/eurekaclient/web/ClientViewController.java
@@ -4,26 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.eurekaclient.services.LifecycleManager;
+import com.example.eurekaclient.services.ServiceInstance;
 import com.example.eurekaclient.services.ServiceInstanceRepository;
 
-    @Controller
-    public class ClientViewController {
-        
-        private final ServiceInstanceRepository repository;
+import java.util.List;
 
-        public ClientViewController(ServiceInstanceRepository repository) {
-            this.repository = repository;
-        }
+@Controller
+public class ClientViewController {
 
-        @GetMapping("/")
-        public String defaultView(Model model) {
-            model.addAttribute("clients", repository.findAll());
-            return "clients"; 
-        }
+    private final ServiceInstanceRepository repository;
+    private final LifecycleManager lifecycleManager;
 
-        @GetMapping("/clients")
-        public String listClients(Model model) {
-            model.addAttribute("clients", repository.findAll());
-            return "clients"; 
-        }
+    public ClientViewController(ServiceInstanceRepository repository,
+                                LifecycleManager lifecycleManager) {
+        this.repository = repository;
+        this.lifecycleManager = lifecycleManager;
+    }
+
+    // Default View "/"
+    @GetMapping("/")
+    public String defaultView(Model model) {
+        return listClients(model);
+    }
+
+    // Clients View "/clients"
+    @GetMapping("/clients")
+    public String listClients(Model model) {
+        List<ServiceInstance> dbClients = repository.findAll();
+        List<ServiceInstance> runningClients = lifecycleManager.getRunningInstances();
+
+        model.addAttribute("dbClients", dbClients);
+        model.addAttribute("runningClients", runningClients);
+
+        return "clients"; 
+    }
 }
